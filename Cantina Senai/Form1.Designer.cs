951303bbca47b65a9f024ff5e772946116f837b2
Campospdp <campospedro187@gmail.com>
2025-05-12 08:02:40 -0300

    Adicionar arquivos de projeto.

---
+++ Cantina Senai/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Cantina_Senai
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            btnAdicionar = new Button();
+            btnQuantidade = new NumericUpDown();
+            btnRemover = new Button();
+            btnFinalizar = new Button();
+            pictureBox1 = new PictureBox();
+            lblTotal = new Label();
+            ((System.ComponentModel.ISupportInitialize)btnQuantidade).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.Info;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(20, 192);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(219, 184);
+            listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = SystemColors.Info;
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(713, 192);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(219, 184);
+            listBox2.TabIndex = 4;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.BackColor = SystemColors.Info;
+            btnAdicionar.Location = new Point(20, 163);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(75, 23);
+            btnAdicionar.TabIndex = 0;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = false;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnQuantidade
+            // 
+            btnQuantidade.BackColor = SystemColors.Info;
+            btnQuantidade.Location = new Point(20, 382);
+            btnQuantidade.Name = "btnQuantidade";
+            btnQuantidade.Size = new Size(120, 23);
+            btnQuantidade.TabIndex = 2;
+            btnQuantidade.ValueChanged += btnQuantidade_ValueChanged;
+            // 
+            // btnRemover
+            // 
+            btnRemover.BackColor = SystemColors.Info;
+            btnRemover.Location = new Point(713, 163);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(75, 23);
+            btnRemover.TabIndex = 3;
+            btnRemover.Text = "Remover";
+            btnRemover.UseVisualStyleBackColor = false;
+            btnRemover.Click += btnRemover_Click;
+            // 
+            // btnFinalizar
+            // 
+            btnFinalizar.BackColor = SystemColors.Info;
+            btnFinalizar.Location = new Point(857, 163);
+            btnFinalizar.Name = "btnFinalizar";
+            btnFinalizar.Size = new Size(75, 23);
+            btnFinalizar.TabIndex = 5;
+            btnFinalizar.Text = "Finalizar";
+            btnFinalizar.UseVisualStyleBackColor = false;
+            btnFinalizar.Click += btnFinalizar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(336, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(265, 321);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.ForeColor = SystemColors.Window;
+            lblTotal.Location = new Point(713, 384);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(16, 15);
+            lblTotal.TabIndex = 11;
+            lblTotal.Text = "...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(944, 497);
+            Controls.Add(lblTotal);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnFinalizar);
+            Controls.Add(btnRemover);
+            Controls.Add(btnQuantidade);
+            Controls.Add(btnAdicionar);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "6";
+            ((System.ComponentModel.ISupportInitialize)btnQuantidade).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button btnAdicionar;
+        private NumericUpDown btnQuantidade;
+        private Button btnRemover;
+        private Button btnFinalizar;
+        private PictureBox pictureBox1;
+        private Label lblTotal;
+    }
+}
---
+++ Cantina Senai/Form1.cs
@@ -0,0 +1,65 @@
+namespace Cantina_Senai
+{
+    public partial class Form1 : Form
+    {
+        private List<Produto> produtos;
+        private Carrinho carrinho;
+        public Form1()
+        {
+            InitializeComponent();
+            carrinho = new Carrinho();
+            listBox1.DisplayMember = "Nome";
+            listBox1.ValueMember = "Preco";
+            listBox2.DisplayMember = "Nome";
+            listBox2.ValueMember = "Preco";
+            listBox1.Items.Add(new Produto { Nome = "Pão de Queijo - R$ 3,50", Preco = 3.50 });
+            listBox1.Items.Add(new Produto { Nome = "Coxinha - R$ 5,00", Preco = 5 });
+            listBox1.Items.Add(new Produto { Nome = "Pastel de Carne - R$ 6,00", Preco = 6 });
+            listBox1.Items.Add(new Produto { Nome = "Pastel de Queijo - R$ 5,50", Preco = 5.50 });
+            listBox1.Items.Add(new Produto { Nome = "Hambúrguer Simples - R$ 8,00", Preco = 8 });
+            listBox1.Items.Add(new Produto { Nome = "Hambúrguer com Queijo - R$ 9,00", Preco = 9 });
+            listBox1.Items.Add(new Produto { Nome = "X-Tudo - R$ 12,00", Preco = 12 });
+            listBox1.Items.Add(new Produto { Nome = "Refrigerante Lata - R$ 4,50", Preco = 4.50 });
+            listBox1.Items.Add(new Produto { Nome = "Suco Natural (300ml) - R$ 4,00", Preco = 4 });
+            listBox1.Items.Add(new Produto { Nome = "Agua Mineral (500ml) - R$ 2,50", Preco = 2.50 });
+
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Produto produto)
+            {
+                carrinho.Adicionar(produto);
+                listBox2.Items.Add(produto);
+                AtualizarTotal();
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem is Produto produto)
+            {
+                carrinho.Remover(produto);
+                listBox2.Items.Remove(produto);
+                AtualizarTotal();
+            }
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Total do Pedido: R${carrinho.Total():F2}", "Pedido Finalizado");
+            carrinho.Limpar();
+            listBox2.Items.Clear();
+            AtualizarTotal();
+        }
+        private void AtualizarTotal()
+        {
+            lblTotal.Text = $"Total: R${carrinho.Total():F2}";
+        }
+
+        private void btnQuantidade_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
